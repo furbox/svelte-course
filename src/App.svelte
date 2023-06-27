501ec9e59b19562e0113f31a39a55ed628a65f6e
Christopher Flores <furbox@gmail.com>
2023-06-27 13:26:27 -0600

    news

--- src/App.svelte
+++ src/App.svelte
@@ -1,60 +1,143 @@
 <script>
   import TodoList from "./lib/TodoList.svelte";
   import { v4 as uuid } from "uuid";
+  import { onMount, tick } from "svelte";
+
   let todoList;
   let showList = true;
-  let todos = [
-    { id: uuid(), text: "Learn Svelte", completed: true },
-    { id: uuid(), text: "Learn React", completed: false },
-    { id: uuid(), text: "Learn Vue", completed: false },
-  ];
 
-  function handleAddTodo(event) {
-    event.preventDefault();
-    todos = [
-      ...todos,
-      {
-        id: uuid(),
-        text: event.detail.text,
-        completed: false,
-      },
-    ];
-    todoList.clearInput();
-  }
+  let todos = null;
+  let error = null;
+  let isLoading = false;
+  let isAdding = false;
+  let disabledItems = [];
 
-  function handleRemoveTodo(event) {
-    todos = todos.filter((t) => t.id !== event.detail.id);
-  }
+  onMount(() => {
+    loadTodos();
+  });
 
-  function handleToggleTodo(event) {
-    todos = todos.map((t) => {
-      if (t.id === event.detail.id) {
-        return {
-          ...t,
-          completed: event.detail.value,
-        };
+  async function loadTodos() {
+    isLoading = true;
+    await fetch("https://jsonplaceholder.typicode.com/todos?_limit=10").then(
+      async (response) => {
+        if (response.ok) {
+          todos = await response.json();
+        } else {
+          error = "An error has occurred";
+        }
       }
-      return t;
+    );
+    isLoading = false;
+  }
+
+  async function handleAddTodo(event) {
+    event.preventDefault();
+    isAdding = true;
+    await fetch("https://jsonplaceholder.typicode.com/todos", {
+      method: "POST",
+      body: JSON.stringify({
+        title: event.detail.title,
+        completed: false,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }).then(async (response) => {
+      if (response.ok) {
+        const todo = await response.json();
+        todos = [...todos, { ...todo, id: uuid() }];
+        todoList.clearInput();
+      } else {
+        error = "An error has occurred";
+      }
     });
+    isAdding = false;
+    await tick();
+    todoList.focusInput();
+  }
+
+  async function handleRemoveTodo(event) {
+    const id = event.detail.id;
+    if (disabledItems.includes(id)) return;
+    disabledItems = [...disabledItems, id];
+    await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+      method: "DELETE",
+    }).then((response) => {
+      if (response.ok) {
+        todos = todos.filter((t) => t.id !== id);
+      } else {
+        error = "An error has occurred";
+      }
+    });
+    disabledItems = disabledItems.filter((i) => i !== id);
+    //todos = todos.filter((t) => t.id !== event.detail.id);
+  }
+
+  async function handleToggleTodo(event) {
+    const id = event.detail.id;
+    const value = event.detail.value;
+    if (disabledItems.includes(id)) return;
+    disabledItems = [...disabledItems, id];
+    await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+      method: "PATCH",
+      body: JSON.stringify({
+        completed: value,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }).then(async (response) => {
+      if (response.ok) {
+        const updateTodo = await response.json();
+        todos = todos.map((t) => {
+          if (t.id === id) {
+            return updateTodo;
+          }
+          return { ...t };
+        });
+      } else {
+        error = "An error has occurred";
+      }
+    });
+    disabledItems = disabledItems.filter((i) => i !== id);
   }
 </script>
 
-<h3>{todos.length} Todos</h3>
-{todoList && todoList.readonly}
-<br />
 <label>
   <input type="checkbox" bind:checked={showList} />
   Show/Hide List
 </label>
 {#if showList}
-  <div style:max-width="300px">
+  <div style:max-width="400px">
     <TodoList
-      bind:this={todoList}
       {todos}
+      {error}
+      {isLoading}
+      {disabledItems}
+      disableAdding={isAdding}
+      bind:this={todoList}
       on:addtodo={handleAddTodo}
       on:removetodo={handleRemoveTodo}
       on:toggletodo={handleToggleTodo}
-    />
+      let:todo
+      let:handleToggleTodo
+      let:index
+    >
+    <svelte:fragment slot="title">{index + 1} - {todo.title}</svelte:fragment>
+    <!--{@const {id, completed, title} = todo}
+      <div>
+        <input
+          disabled={disabledItems.includes(id)}
+          on:input={(event) => {
+            event.currentTarget.checked = completed;
+            handleToggleTodo(id, !completed);
+          }}
+          type="checkbox"
+          checked={completed}
+        />
+        {title}
+      </div>-->
+    </TodoList>
   </div>
 {/if}
 
--- src/lib/TodoList.svelte
+++ src/lib/TodoList.svelte
@@ -2,41 +2,29 @@
 
 <script>
   import Button from "./Button.svelte";
-  import {
-    createEventDispatcher,
-    onDestroy,
-    onMount,
-    beforeUpdate,
-    afterUpdate,
-  } from "svelte";
-
-  onMount(() => {
-    console.log("Mounted");
-    return () => {
-      console.log("Destroyed 2");
-    };
-  });
-
-  onDestroy(() => {
-    console.log("Destroyed");
-  });
-
-  beforeUpdate(() => {
-    if (listDiv) {
-      console.log(listDiv.offsetHeight);
-    }
-  });
+  import { createEventDispatcher, afterUpdate } from "svelte";
+  import FaRegTrashAlt from "svelte-icons/fa/FaRegTrashAlt.svelte";
+  import { identity } from "svelte/internal";
 
   afterUpdate(() => {
-    if (autoScroll) listDiv.scrollTo(0, listDiv.scrollHeight)
+    if (autoScroll) listDiv.scrollTo(0, listDivScrollHeight);
     autoScroll = false;
   });
 
-  export let todos = [];
+  export let todos = null;
+  export let error = null;
+  export let isLoading = false;
+  export let disableAdding = false;
+  export let disabledItems = [];
+
   let prevTodos = todos;
-  $:{
-    autoScroll = todos.length > prevTodos.length;
-    console.log(prevTodos, todos) 
+  let inputText = "";
+  let input, listDiv, autoScroll, listDivScrollHeight;
+
+  const dispatch = createEventDispatcher();
+
+  $: {
+    autoScroll = todos && prevTodos && todos.length > prevTodos.length;
     prevTodos = todos;
   }
   export function clearInput() {
@@ -45,16 +33,12 @@
   export function focusInput() {
     input.focus();
   }
-  let inputText = "";
-  let input, listDiv, autoScroll;
-
-  const dispatch = createEventDispatcher();
 
   function handleAddTodo() {
     const isNotCancelled = dispatch(
       "addtodo",
       {
-        text: inputText,
+        title: inputText,
       },
       { cancelable: true }
     );
@@ -69,7 +53,7 @@
     });
   }
 
-  function handleToggleComplete(id, value) {
+  function handleToggleTodo(id, value) {
     dispatch("toggletodo", {
       id,
       value,
@@ -78,36 +62,149 @@
 </script>
 
 <div class="todo-list-wrapper">
-  <div class="todo-list" bind:this={listDiv}>
-    <ul>
-      {#each todos as { id, text, completed } (id)}
-        <li>
-          <label>
-            <input
-              on:input={(event) => {
-                event.currentTarget.checked = completed;
-                handleToggleComplete(id, !completed);
-              }}
-              type="checkbox"
-              checked={completed}
-            />
-            {text}
-          </label>
-          <button on:click={() => handleDeleteTodo(id)}>Delete</button>
-        </li>
-      {/each}
-    </ul>
-  </div>
-  {inputText}
+  {#if isLoading}
+    <p class="state-text">Loading...</p>
+  {:else if error}
+    <p class="state-text">Error: {error}</p>
+  {:else if todos}
+    <div class="todo-list" bind:this={listDiv}>
+      <div bind:offsetHeight={listDivScrollHeight}>
+        {#if todos.length === 0}
+          <p class="state-text">No todos yet</p>
+        {:else}
+          <ul>
+            {#each todos as todo, index (todo.id)}
+            {@const {id, completed, title} = todo}
+              <li>
+                <slot {todo} {index} {handleToggleTodo}>
+                  <div class:completed>
+                    <label>
+                      <input
+                        disabled={disabledItems.includes(id)}
+                        on:input={(event) => {
+                          event.currentTarget.checked = completed;
+                          handleToggleTodo(id, !completed);
+                        }}
+                        type="checkbox"
+                        checked={completed}
+                      />
+                      <slot name="title">{title}</slot>
+                    </label>
+                    <button
+                      disabled={disabledItems.includes(id)}
+                      class="remove-todo-button"
+                      aria-label="Remove Todo: {title}"
+                      on:click={() => handleDeleteTodo(id)}
+                      ><span style:width="10px" style:display="inline-block"
+                        ><FaRegTrashAlt /></span
+                      ></button
+                    >
+                  </div>
+                </slot>
+              </li>
+            {/each}
+          </ul>
+        {/if}
+      </div>
+    </div>
+  {/if}
   <form class="add-todo-form" on:submit|preventDefault={handleAddTodo}>
-    <input bind:this={input} bind:value={inputText} />
-    <Button type="submit" disabled={!inputText}>Add</Button>
+    <input
+      disabled={disableAdding || !todos}
+      placeholder="New Todo"
+      bind:this={input}
+      bind:value={inputText}
+    />
+    <Button
+      class="add-todo-button"
+      type="submit"
+      disabled={!inputText || disableAdding || !todos}>Add</Button
+    >
   </form>
 </div>
 
-<style>
-  .todo-list{
-    max-height: 150px;
-    overflow: auto;
+<style lang="scss">
+  .todo-list-wrapper {
+    background-color: #424242;
+    border: 1px solid #4b4b4b;
+    .state-text {
+      margin: 0;
+      padding: 15px;
+      text-align: center;
+    }
+    .todo-list {
+      max-height: 150px;
+      overflow: auto;
+      ul {
+        margin: 0;
+        padding: 10px;
+        list-style: none;
+        li > div {
+          margin-bottom: 5px;
+          display: flex;
+          align-items: center;
+          background-color: #303030;
+          border-radius: 5px;
+          padding: 10px;
+          position: relative;
+          label {
+            cursor: pointer;
+            font-size: 19;
+            display: flex;
+            color: #fff;
+            align-items: baseline;
+            padding-right: 20px;
+            input[type="checkbox"] {
+              margin: 0 10px 0 0;
+              cursor: pointer;
+            }
+          }
+          &.completed > label {
+            opacity: 0.5;
+            text-decoration: line-through;
+          }
+          .remove-todo-button {
+            border: none;
+            background: none;
+            padding: 5px;
+            position: absolute;
+            right: 10px;
+            cursor: pointer;
+            display: none;
+            &:disabled {
+              opacity: 0.4;
+              cursor: not-allowed;
+            }
+            :global(svg) {
+              fill: red;
+            }
+          }
+          &:hover {
+            .remove-todo-button {
+              display: block;
+            }
+          }
+        }
+      }
+    }
+    .add-todo-form {
+      padding: 15px;
+      background-color: #303030;
+      display: flex;
+      flex-wrap: wrap;
+      border-top: 1px solid #4b4b4b;
+      //:global(.add-todo-button){
+      //  background-color: aqua;
+      //}
+      input {
+        flex: 1;
+        background-color: #424242;
+        border: 1px solid #4b4b4b;
+        padding: 10px;
+        color: #fff;
+        border-radius: 5px;
+        margin-right: 10px;
+      }
+    }
   }
 </style>
